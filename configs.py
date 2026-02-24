ab92e36e433a075dddfb829fef8b90ba9e974ba8
M.Kh <sea-of-max@yandex.ru>
2026-02-24 22:14:49 +0300

    reconstruction

--- configs.py
+++ configs.py
@@ -1,9 +1,9 @@
-TOKEN = '6050867287:AAGGG2blD609bc8ELrquhAoWcJA-ITtsd0w'
-currency_dict = {
-    'рубль': 'RUB',
-    'доллар': 'USD',
-    'евро': 'EUR',
-    'бр.фунт': 'GBP',
-    'яп.йена': 'JPY',
-    'кит.юань': 'CNY'
-}
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("TOKEN")
+
+if not token:
+    raise ValueError("Нет токена. Проверь авторизацию.")
--- task_18_6_chatbot.py
+++ task_18_6_chatbot.py
@@ -1,30 +1,28 @@
 import telebot
-from configs import TOKEN, currency_dict
-from extentions import APIException, ConverterTool
+from configs import token
+from exceptions import APIException, ConverterTool
 
-currency_Bot = telebot.TeleBot(TOKEN)
+HomeAssistant_Bot = telebot.TeleBot(token)
 
-@currency_Bot.message_handler(commands=['start'])
+@HomeAssistant_Bot.message_handler(commands=['start'])
 def bot_start(message):
-            text = 'Привет!\nВы запустили бот-помошник для конвертации валют.' \
+            text = 'Привет!\nВы запустили бот-помошник по дому.' \
                    '\nПравила использования и список доступных команд можете увидеть набрав команду: ' \
                    ' /help'
-            currency_Bot.send_message(message.chat.id, text)
-@currency_Bot.message_handler(commands=['help'])
+            HomeAssistant_Bot.send_message(message.chat.id, text)
+@HomeAssistant_Bot.message_handler(commands=['help'])
 def help_cmnd(message):
-    text = '1. Чтобы сконвертировать валюту по текущему курсу, введите без кавычек через пробел:' \
-           '\n<валюта, из которой переводим>, < валюта в которую переводим>, <количество единиц валюты>.' \
+    text = '1. Чтобы ...:' \
+           '\n<...>, <...>, <...>.' \
            '\n' \
-           '\n2. Наименование валют для запроса конвертации смотрите в списке доступных по команде: /values'
-    currency_Bot.send_message(message.chat.id, text)
-@currency_Bot.message_handler(commands=['values'])
-def curency_rates(message):
-    text = 'Доступные валюты для перевода: '
-    for k in currency_dict.keys():
-        text = '\n'.join((text, k, ))
-    currency_Bot.send_message(message.chat.id, text)
+           '\n2. Что-то второе: /values'
+    HomeAssistant_Bot.send_message(message.chat.id, text)
 
-@currency_Bot.message_handler(content_types=['text'])
+# @HomeAssistant_Bot.message_handler(commands=['values'])
+# def values_graphs(message):
+
+
+@HomeAssistant_Bot.message_handler(content_types=['text'])
 def currency_convert(message: telebot.types.Message):
     try:
         values = message.text.split(' ')
@@ -35,11 +33,11 @@ def currency_convert(message: telebot.types.Message):
             base, qoute, amount = values
             result = ConverterTool.get_price(base, qoute, amount)
     except APIException as er:
-        currency_Bot.reply_to(message, f'Ошибка ввода, проверьте данные. Ошибка - {er}')
+        HomeAssistant_Bot.reply_to(message, f'Ошибка ввода, проверьте данные. Ошибка - {er}')
     except Exception as er:
-        currency_Bot.reply_to(message, f'Невозможно обработать команду, ошибка - {er}')
+        HomeAssistant_Bot.reply_to(message, f'Невозможно обработать команду, ошибка - {er}')
     else:
         text_res = f'количество {amount} {base} в {qoute} по курсу {result} = {round(result*float(amount), 3)}'
-        currency_Bot.send_message(message.chat.id, text_res)
+        HomeAssistant_Bot.send_message(message.chat.id, text_res)
 
-currency_Bot.polling(none_stop=True)
+HomeAssistant_Bot.polling(none_stop=True)
